9044529d7f60cad025561cc9dd23b4cb771526b7
Sanginfo-Shailesh <shailesh.saindane@sanginfo.com>
2018-08-21 16:03:53 +0530

    First Commit

---
+++ arithmetic-lib/src/main/java/com/arithmetic_lib/MyMath.java
@@ -0,0 +1,25 @@
+package com.arithmetic_lib;
+
+public class MyMath {
+
+    public static int Plus(int a, int b) {
+        return a + b;
+    }
+
+    public static int Minus(int a, int b) {
+        return a - b;
+    }
+
+    public static int Times(int a, int b) {
+        return a * b;
+    }
+
+    public static float Div(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Div by Zero error......");
+        } else {
+            return a / b;
+        }
+    }
+
+}
