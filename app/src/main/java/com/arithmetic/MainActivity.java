60212998039f66b6082ad8243b135b215a128aa4
Sanginfo-Shailesh <shailesh.saindane@sanginfo.com>
2018-08-23 12:19:32 +0530

    Changes

--- app/src/main/java/com/arithmetic/MainActivity.java
+++ app/src/main/java/com/arithmetic/MainActivity.java
@@ -2,12 +2,20 @@ package com.arithmetic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.arithmetic_lib.MyMath;
 
 public class MainActivity extends AppCompatActivity {
+
+    int added;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        added = MyMath.Plus(10, 10);
+        Toast.makeText(getApplicationContext(), "Addition : " + added, Toast.LENGTH_SHORT).show();
     }
 }
--- arithmetic-lib/src/main/java/com/arithmetic_lib/MyMath.java
+++ arithmetic-lib/src/main/java/com/arithmetic_lib/MyMath.java
@@ -2,23 +2,30 @@ package com.arithmetic_lib;
 
 public class MyMath {
 
+    private static int add, subtract, multiply;
+    private static float division;
+
     public static int Plus(int a, int b) {
-        return a + b;
+        add = a + b;
+        return add;
     }
 
     public static int Minus(int a, int b) {
-        return a - b;
+        subtract = a - b;
+        return subtract;
     }
 
     public static int Times(int a, int b) {
-        return a * b;
+        multiply = a * b;
+        return multiply;
     }
 
     public static float Div(int a, int b) {
         if (b == 0) {
             throw new IllegalArgumentException("Div by Zero error......");
         } else {
-            return a / b;
+            division = a / b;
+            return division;
         }
     }
 
